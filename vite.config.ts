d6e4e20def5b7f92875d1786f7cf4df075b5a955
Alexandr <ooo647kn@gmail.com>
2024-12-01 17:49:39 +0300

    2

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
   plugins: [react(), svgr()],
   server: {
     host: '0.0.0.0',
-    port: Number(process.env.PORT) || 3000
+    port: process.env.PORT ? Number(process.env.PORT) : 3000
+  },
+  build: {
+    sourcemap: false,
+    minify: true
   }
 })
