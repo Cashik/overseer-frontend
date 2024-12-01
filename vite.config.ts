7680acdc97257d47425a490611e062cca2c3f746
Alexandr <ooo647kn@gmail.com>
2024-12-01 17:55:33 +0300

    q123

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-/// <reference types="node" />
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   server: {
